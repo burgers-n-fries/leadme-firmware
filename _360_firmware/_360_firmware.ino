37046b84c094e36e2b20af0516a58a7eefb753cf
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2015-01-05 14:02:51 -0500

    Shiny new firmware for v2.0

--- _360_firmware/_360_firmware.ino
+++ _360_firmware/_360_firmware.ino
@@ -1,25 +1,43 @@
 /*
-Number of sensor is set by num_vib.
-Serial command is "angle@[angle]" where [angle] is 0-359 (or more, it's moded, don't worry).
-Once given, the approriate thingies vibrate.  They are faded if the angle is not a multiple of (360/num_vib).
-The first 6 are the 6 PWM pins, in increasing number order.
-If more are needed, we'll serial over a command to a second chip.  It'll be good.
-If you send a negative number, they all stop.
-Enjoy.
-D
+Blue
+by dimitdim
+
+LeadMe firmware
+for hardware v2.0
+
+Settings below:
+- Pin numbers: should be self-explanatory.  Digital Arduino pin numbers unless noted.  Mostly for testing
+- batt_low: when should the LED starting blinking?
+- num_vib: number of vibrators.  Mostly or testing
+- LED_enable: turn LED off it it proves annoying
+
+Serial commands:
+- angle@[angle]!: vibrate in [angle] direction, or turn off all motors if [angle]<0
+- all!: vibrate all motors
+- test!: demo mode, rotate motors
+- batt!: reutn battery level in percent
+- motor@[val]! vibrate a specific motor at a specfic speed.  0-255 vibrates motor 0, 256-511 vibrates motor 1, etc. Mostly for testing
+All commands also return current PWM values.  Mostly for testing
+
 */
 
-const int num_vib=5;
+/*Pin Definitions*/
+const int M0 = 5; //Leftmost bird's-eye-view.  Increasing clockwise...
+const int M1 = 6;
+const int M2 = 9;
+const int M3 = 10;
+const int M4 = 11;
 
-int duty[num_vib]={0};
-int angle=-1;
-int base=0;
-int add=0;
-int i;
+const int BatLvl = 0; //Analog
+const int Butt = 12;
+const int Charge = 8; //The IC outputs tri-state, so I can change this to an ADC input, stick a voltage divider on there and differentiate between "Not charging" and "Done charging". On popular demand.
+const int BTStat = 7;
+const int LED = 13;
 
-int battery_level=100;
-boolean demo=false;
-int demo_time;
+/*Settings*/
+const int batt_low = 20; //In percent
+const int num_vib = 5; //Legacy, pretty sure we're set on 5.  Helpful for testing though...
+const boolean LED_enable = true;
 
 /*Serial*/
 String inputString=""; //Initiate Serial input command variables
@@ -27,8 +45,38 @@ String cmd="";
 int val=0;
 boolean stringComplete=false; //Don't run debuging info until a command is entered
 
-void update_bl() {
+/*Misc Definitions*/
+int duty[num_vib]={0};
+int angle=-1;
+int base=0;
+int add=0;
+int i;
+int battery_level;
+int bl_raw;
+boolean demo=false;
+int time10;
+int demo_time;
+
+void all(int d) {
+  for(i=0;i<num_vib;i++) {
+    duty[i]=d;
+  }
 }
+
+void update_bl() {
+  bl_raw=analogRead(BatLvl);
+  battery_level = 100*((BatLvl-574)/62); //Apologies for the random hard-coded numbers.  We'll get an expirimental curve up in this bitch later
+  battery_level = max(0,min(100,battery_level)); //Some limiting
+}
+
+void update_LED() { if(LED_enable) {
+  if(!digitalRead(Charge)) digitalWrite(LED,(time10%2000)>1000); //Can change to pulsing on popular demand.  But the LED won't be D13, and that sucks cuz it's usefull for SPI
+  else digitalWrite(LED,digitalRead(BTStat));
+  if(battery_level<=batt_low && time10%5000<600) {
+    if((time10%200)<100) digitalWrite(LED,HIGH);
+    else digitalWrite(LED,LOW);
+  }
+}}
 
 void communicate() {
   cmd=inputString.substring(0,inputString.indexOf('@')); //Split the input into command and value. The only reason this isn't '=' is because I like '@'
@@ -37,9 +85,7 @@ void communicate() {
   
   if (cmd.equals(String("angle"))) {
     angle=(max(-1,val))%360;
-    for(i=0;i<num_vib;i++) {
-      duty[i]=0;
-    }
+    all(0);
     if(angle>=0) {
       base=angle/(360/num_vib);
       add=angle%(360/num_vib);
@@ -47,20 +93,26 @@ void communicate() {
       duty[(base+1)%num_vib]=(255*add)/(360/num_vib);
     }
   }
-  
-  if (cmd.equals(String("test"))) {
-    demo=true;
-    Serial.print("Demo Time!");
+
+  else if (cmd.equals(String("motor"))) {
+    duty[(val/255)%num_vib]=val%255;
   }
   
-  if(cmd.equals(String("all"))) {
-    for(i=0;i<num_vib;i++) {
-      duty[i]=255;
-    }
+  else if (cmd.equals(String("test"))) {
+    demo=true;
+    Serial.println("Demo Time!");
+  }
+  
+  else if(cmd.equals(String("all"))) {
+    all(255);
   }
 
-  if(cmd.equals(String("batt"))) {
-    Serial.print(battery_level);
+  else if(cmd.equals(String("batt"))) {
+    Serial.println(battery_level);
+  }
+
+  else {
+    Serial.println("Try again.");
   }
 
   Serial.print("PWM Values: ");
@@ -75,34 +127,45 @@ void communicate() {
 }
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {;} //some Arduinos take a while...
+  while (!Serial) {;} //some Arduinos take a while...  Leonardo, I'm looking at you...
   inputString.reserve(128); //allows adding one byte at a time (it needs to be a buffer of sorts)
-  for(i=0;i<14;i++) { //set them all to outputs
-    pinMode(i,OUTPUT);
+  pinMode(M0,OUTPUT);
+  pinMode(M1,OUTPUT);
+  pinMode(M2,OUTPUT);
+  pinMode(M3,OUTPUT);
+  pinMode(M4,OUTPUT);
+  pinMode(BatLvl,INPUT);
+  pinMode(Charge,INPUT);
+  pinMode(LED,OUTPUT);
+  Serial.println("Welcome!");
+  for(i=0;i<3;i++) { //Welcome!
+    all(255);
+    digitalWrite(LED,HIGH);
+    delay(250);
+    all(0);
+    digitalWrite(LED,LOW);
+    delay(250);
   }
 }
 
 void loop() {
+  time10=millis()%10000;
   update_bl();
+  update_LED();
   if(stringComplete) {communicate();} //update PWM duty cycles
   if(demo) {
-    demo_time=(millis()%1000)/4;
-    analogWrite(3,((demo_time+0*255/num_vib)%255));
-    analogWrite(5,((demo_time+1*255/num_vib)%255));
-    analogWrite(6,((demo_time+2*255/num_vib)%255));
-    analogWrite(9,((demo_time+3*255/num_vib)%255));
-    analogWrite(10,((demo_time+4*255/num_vib)%255));
+    demo_time=(time10%1000)/4;
+    duty[0]=(demo_time+0*255/num_vib)%255;
+    duty[1]=(demo_time+1*255/num_vib)%255;
+    duty[2]=(demo_time+2*255/num_vib)%255;
+    duty[3]=(demo_time+3*255/num_vib)%255;
+    duty[4]=(demo_time+4*255/num_vib)%255;
   }
-  else {
-    analogWrite(3,duty[0]);
-    analogWrite(5,duty[1]);
-    analogWrite(6,duty[2]);
-    analogWrite(9,duty[3]);
-    analogWrite(10,duty[4]);
-  }
-  //analogWrite(11,duty[5]);
-  //digitalWrite(12,duty[6]);
-  //digitalWrite(13,duty[7]);
+  analogWrite(M0,duty[0]);
+  analogWrite(M1,duty[1]);
+  analogWrite(M2,duty[2]);
+  analogWrite(M3,duty[3]);
+  analogWrite(M4,duty[4]);
 }
 
 void serialEvent() { //This gets run every time after loop
